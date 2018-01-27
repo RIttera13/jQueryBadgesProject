977e982742e8515ca69848e57023f24942e394d9
Andrew Ritter <andrewritter@wesellmeat.com>
2018-01-27 14:12:04 -0600

    Completed, second push

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,15 +1,15 @@
 $(function() {
-  var json = [];
   $.ajax({
     url: 'https://www.codeschool.com/users/rittera13.json',
     dataType: 'jsonp',
     success: function(response) {
       json = response;
       $.each(json.courses.completed, function(index, value) {
-        $("#badges").append(
-          "<div class='course'><h3>" + value.title + "</h3><img src='" + value.badge + "'/><a class='btn btn-primary' href='" + value.url + "' target='_blank' >See Course</a>'</div>" );
+        $("#badges").append("<div class='course' id='course_" + index + "'>");
+        $("#course_" + index).append("<h3>" + value.title + "</h3>");
+        $("#course_" + index).append("<img src='" + value.badge + "'/>");
+        $("#course_" + index).append("<a class='btn btn-primary' href='" + value.url + "' target='_blank'>See Course</a>");
       });
     }
   });
-
 });
